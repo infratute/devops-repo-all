f7d65fb051365f6634b55cf904ec3d4139207e7f
Infratute IT Labs <87716160+infratute@users.noreply.github.com>
2022-11-23 20:28:30 +0530

    Update 01_jenkins-setup.sh

--- 01_jenkins-setup.sh
+++ 01_jenkins-setup.sh
@@ -9,5 +9,5 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt-get install jenkins -y
+sudo apt-get install jenkins
 ###

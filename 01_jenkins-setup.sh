55c07d2931418fdf7244a3d25116bc9250e9e644
Infratute IT Labs <87716160+infratute@users.noreply.github.com>
2022-12-23 19:08:25 +0530

    Update 01_jenkins-setup.sh

--- 01_jenkins-setup.sh
+++ 01_jenkins-setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # USE UBUNTU20.04 - INSTANCE: 2GB RAM + 2VCPU MIN - WILL ONLY WORK
-sudo apt update
+sudo apt update -y
 sudo apt install openjdk-11-jdk -y
+sudo apt update -y
+sudo apt install openjdk-8-jdk -y
 sudo apt install maven -y
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get update -y
+sudo apt-get install jenkins -y
 ###

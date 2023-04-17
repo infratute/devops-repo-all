9fdedc385647151c252affcc06d7bdb21124fec2
Infratute IT Labs <87716160+infratute@users.noreply.github.com>
2023-04-17 19:17:58 +0530

    gpg keys - latest one added

--- 01_jenkins-setup.sh
+++ 01_jenkins-setup.sh
@@ -5,7 +5,7 @@ sudo apt install openjdk-11-jdk -y
 sudo apt update -y
 sudo apt install openjdk-8-jdk -y
 sudo apt install maven -y
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \

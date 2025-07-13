f56f4d408484aadf289b5dbe7ac540f135660b10
Infratute IT Labs <87716160+infratute@users.noreply.github.com>
2025-07-13 19:00:06 +0530

    Add files via upload
    New Nexus Setup File Added

---
+++ 03_Nexu_setup-new.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# This script works on Centos 9 also. Line 7 - Check for the correct download url from the below link.
+# https://help.sonatype.com/en/download-archives---repository-manager-3.html
+yum install java-17-openjdk wget rsync -y   
+mkdir -p /opt/nexus/   
+mkdir -p /tmp/nexus/                           
+cd /tmp/nexus/
+NEXUSURL="https://download.sonatype.com/nexus/3/nexus-3.81.1-01-linux-x86_64.tar.gz"
+wget $NEXUSURL -O nexus.tar.gz
+EXTOUT=`tar xzvf nexus.tar.gz`
+NEXUSDIR=`echo $EXTOUT | cut -d '/' -f1`
+rm -rf /tmp/nexus/nexus.tar.gz
+rsync -avzh /tmp/nexus/ /opt/nexus/
+useradd nexus
+chown -R nexus.nexus /opt/nexus
+cat <<EOT>> /etc/systemd/system/nexus.service
+[Unit]                                                                          
+Description=nexus service                                                       
+After=network.target                                                            
+                                                                  
+[Service]                                                                       
+Type=forking                                                                    
+LimitNOFILE=65536                                                               
+ExecStart=/opt/nexus/$NEXUSDIR/bin/nexus start                                  
+ExecStop=/opt/nexus/$NEXUSDIR/bin/nexus stop                                    
+User=nexus                                                                      
+Restart=on-abort                                                                
+                                                                  
+[Install]                                                                       
+WantedBy=multi-user.target                                                      
+
+EOT
+
+echo 'run_as_user="nexus"' > /opt/nexus/$NEXUSDIR/bin/nexus.rc
+systemctl daemon-reload
+systemctl start nexus
+systemctl enable nexus
